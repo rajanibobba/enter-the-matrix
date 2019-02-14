d1d0a49e24d406eefde3b5a393956a03748b6bf5
Bobba <Rajani.Bobba@McKesson.com>
2019-02-14 10:35:29 -0500

    rajanis matrices

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from vector import Vector
 from matrix import Matrix
+from functools import reduce
 
-m1 = Matrix.random(3, 4)
+# m1 = Matrix.random(3, 4)
 
 # 1 cry
 # 2 goto 1
@@ -8,10 +10,64 @@ m1 = Matrix.random(3, 4)
 # filter the 4x2 and 2x4 matrices into
 # different lists
 
+mat_from_file_list = []
+vectors = []
+
+mat_four_by_two_list = []
+mat_two_by_four_list = []
+with open('matrix.txt') as f:
+   for line in f:
+       if (line != '\n'):
+           vectors.append(Vector(*tuple(map(int, line.strip().split(' ')))))
+       else:
+           mat_from_file_list.append(Matrix(*tuple(vectors)))
+           vectors = []
+   mat_from_file_list.append(Matrix(*tuple(vectors)))
+
+for mat in mat_from_file_list:
+   if (mat.get_dim() == (4, 2)):
+       mat_four_by_two_list.append(mat)
+   else:
+       mat_two_by_four_list.append(mat)
+
+m421 = mat_four_by_two_list[0] 
+m422 = mat_four_by_two_list[1]
+
+m241 = mat_two_by_four_list[0]
+
 # add any 2 compatible matrices
+print(m421)
+print(m422)
+print(m421 + m422)
+
 # multiply any 2 compatible matrices
-# scale any matrix
+print(m421.dot(m241))
+
 # create a list of 5 3x3 matrices of random values
+mat_list  = []
+for i in range(5):
+   mat_list.append(Matrix.random(3, 3))
+
+#for m in mat_list:
+#    print(m)     
+
 # and add them together
+#print("Final matrix after adding all random matrices")
+final = reduce(lambda acc, mat: acc + mat, mat_list, Matrix(Vector(0, 0, 0), Vector(0, 0, 0), Vector(0, 0, 0)))
+#print(final)
+
+# scale any matrix
+#print("Final matrix after scale by 3")
+s3 = final.scale(3)
+#print(fs3)
 
 # multiply a matrix times a vector
+m1 = mat_four_by_two_list[0]
+#print("Matrix 1 ")
+print(m1)
+mv = m1.times_vector(Vector(2, 2, 2))
+#rint(mv)
+
+#transform a matrix
+print("Transformed Matrix")
+print(list(zip(*m1.vecs)))
--- matrix.py
+++ matrix.py
@@ -5,9 +5,47 @@
 # scale matrix
 # create a random mxn matrix
 # with values between 0 and 1
-
+from vector import Vector
+import random as rand
+from functools import reduce
+import itertools 
 
 class Matrix:
-    @staticmethod
-    def random(params):
-        pass
+   @staticmethod
+   def random(r, c):
+       m = []
+       for i in range(r):
+           r1 = Vector(*tuple([rand.random() for _ in range(c)]))
+           m.append(r1)
+       return Matrix(*tuple(m))
+
+   def __init__(self, *vecs):
+       self.vecs = vecs
+
+   def __str__(self):
+       output = reduce(lambda output, vec: output + str(vec) + '\n', self.vecs, '[') + ']'
+       return output
+
+   def __add__(self, other):
+       return Matrix(*[c1 + c2 for c1, c2 in zip(self.vecs, other.vecs)])
+
+   def scale(self, scalar):
+       return Matrix(*[vec.scale(scalar) for vec in self.vecs])
+
+   def get_dim(self):
+       return len(self.vecs), len(self.vecs[0])
+
+   def times_vector(self, vec):
+        if (self.get_dim()[1] != len(vec)):
+            return "Not Possible"
+        else:    
+            return([v.dot(vec) for v in self.vecs])
+
+   def dot(self, other):
+        if (self.get_dim()[1] != other.get_dim()[0]):
+            return "Multiplication Not Possible"
+        else:    
+            return(1)
+
+   def transform(self):
+      return (list(zip(*self.vecs)))
